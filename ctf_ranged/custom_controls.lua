3580ef30a7cbf7299bdae53597a0d8ed1ab01a38
Apollo <beanzilla@beanzilla.net>
2022-03-25 17:34:51 -0400

    feat(custom_controls.lua): no luck adding scope zoom for weapons under MineClone
    I was thinking it could be simply just use code from mcl_spyglass but nope... no luck.

--- ctf_ranged/custom_controls.lua
+++ ctf_ranged/custom_controls.lua
@@ -15,35 +15,73 @@ local function binoculars_override(player)
       if old_binoculars_update ~= nil then
          old_binoculars_update(player)
       end
+      if minetest.get_modpath("mcl_core") ~= nil then
+         minetest.log("action", "[ctf_ranged] no scope")
+         player:set_fov(86.1)
+      end
       return
    end
 
    -- Only set property if necessary to avoid player mesh reload
-   if player:get_properties().zoom_fov ~= scope_zoom then
-      player:set_properties({zoom_fov = scope_zoom})
+   if minetest.get_modpath("mcl_core") == nil then
+      if player:get_properties().zoom_fov ~= scope_zoom then
+         player:set_properties({zoom_fov = scope_zoom})
+         return
+      end
+   else
+      --minetest.log("action", "[ctf_ranged] "..player:get_player_name().." fov="..tostring(player:get_fov()))
+      if player:get_fov() ~= scope_zoom then
+         minetest.log("action", "[ctf_ranged] scope found")
+         player:set_fov(scope_zoom, false, 0.1)
+         return
+      end
+   end
+end
+
+-- Zooms into view
+local function enable_scope(player)
+   local w_item = player:get_wielded_item()
+   local scope_zoom = w_item:get_definition().ctf_guns_scope_zoom
+   if scope_zoom == nil then
       return
    end
+   minetest.log("action", "[ctf_ranged] "..player:get_player_name().." zooming in with "..tostring(scope_zoom))
+   player:set_fov(scope_zoom, false, 0.1)
+end
 
-
+-- Zooms out of view
+local function disable_scope(player)
+   player:set_fov(86.1)
 end
 
 minetest.register_on_mods_loaded(function()
+   minetest.log("action", "[ctf_ranged] Processing controls...")
+      local use_binoculars = false
       if minetest.get_modpath("binoculars") then
+         use_binoculars = true
          old_binoculars_update = binoculars.update_player_property
          binoculars.update_player_property = binoculars_override
       end
 
       controls.register_on_press(function(player, control_name)
-	    if control_name ~= "zoom" then
+	    if control_name ~= "zoom" or control_name ~= "RMB" then
 	       return
 	    end
-	    binoculars_override(player)
+       if use_binoculars then
+	      binoculars_override(player)
+       else
+         enable_scope(player)
+       end
       end)
       controls.register_on_release(function(player, control_name, time)
-	    if control_name ~= "zoom" then
+       if control_name ~= "zoom" or control_name ~= "RMB" then
 	       return
 	    end
-	    binoculars_override(player)
+       if use_binoculars then
+	      binoculars_override(player)
+       else
+         disable_scope(player)
+       end
       end)
 
 end)
